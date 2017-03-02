5e1ce0ec5257a13045549dfbc16889629f1f2441
Jonathan Perry <shodiwarmic@gmail.com>
2017-03-02 13:12:54 -0500

    Add project files.

---
+++ WindowsApplication1/LanSchoolClassListsHelper.Designer.vb
@@ -0,0 +1,336 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class LanSchoolClassListsHelper
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LoginNameRadioButton = New System.Windows.Forms.RadioButton()
+        Me.MachineNameRadioButton = New System.Windows.Forms.RadioButton()
+        Me.ADNameRadioButton = New System.Windows.Forms.RadioButton()
+        Me.LoginNameComboBox = New System.Windows.Forms.ComboBox()
+        Me.MachineNameComboBox = New System.Windows.Forms.ComboBox()
+        Me.ADNameComboBox = New System.Windows.Forms.ComboBox()
+        Me.ClassNameListBox = New System.Windows.Forms.ListBox()
+        Me.StudentListBox = New System.Windows.Forms.ListBox()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.FolderPathTextBox = New System.Windows.Forms.TextBox()
+        Me.BrowseButton = New System.Windows.Forms.Button()
+        Me.LoadButton = New System.Windows.Forms.Button()
+        Me.LoginNameAddNewButton = New System.Windows.Forms.Button()
+        Me.MachineNameAddNewButton = New System.Windows.Forms.Button()
+        Me.ADNameAddNewButton = New System.Windows.Forms.Button()
+        Me.AddNewClassButton = New System.Windows.Forms.Button()
+        Me.AddNewStudentButton = New System.Windows.Forms.Button()
+        Me.ClassCountLabel = New System.Windows.Forms.Label()
+        Me.StudentCountLabel = New System.Windows.Forms.Label()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(461, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OpenToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'OpenToolStripMenuItem
+        '
+        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(112, 22)
+        Me.OpenToolStripMenuItem.Text = "Open..."
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(112, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(107, 22)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'LoginNameRadioButton
+        '
+        Me.LoginNameRadioButton.AutoSize = True
+        Me.LoginNameRadioButton.Enabled = False
+        Me.LoginNameRadioButton.Location = New System.Drawing.Point(12, 68)
+        Me.LoginNameRadioButton.Name = "LoginNameRadioButton"
+        Me.LoginNameRadioButton.Size = New System.Drawing.Size(82, 17)
+        Me.LoginNameRadioButton.TabIndex = 4
+        Me.LoginNameRadioButton.TabStop = True
+        Me.LoginNameRadioButton.Text = "Login Name"
+        Me.LoginNameRadioButton.UseVisualStyleBackColor = True
+        '
+        'MachineNameRadioButton
+        '
+        Me.MachineNameRadioButton.AutoSize = True
+        Me.MachineNameRadioButton.Enabled = False
+        Me.MachineNameRadioButton.Location = New System.Drawing.Point(12, 104)
+        Me.MachineNameRadioButton.Name = "MachineNameRadioButton"
+        Me.MachineNameRadioButton.Size = New System.Drawing.Size(97, 17)
+        Me.MachineNameRadioButton.TabIndex = 7
+        Me.MachineNameRadioButton.TabStop = True
+        Me.MachineNameRadioButton.Text = "Machine Name"
+        Me.MachineNameRadioButton.UseVisualStyleBackColor = True
+        '
+        'ADNameRadioButton
+        '
+        Me.ADNameRadioButton.AutoSize = True
+        Me.ADNameRadioButton.Enabled = False
+        Me.ADNameRadioButton.Location = New System.Drawing.Point(12, 140)
+        Me.ADNameRadioButton.Name = "ADNameRadioButton"
+        Me.ADNameRadioButton.Size = New System.Drawing.Size(131, 17)
+        Me.ADNameRadioButton.TabIndex = 10
+        Me.ADNameRadioButton.TabStop = True
+        Me.ADNameRadioButton.Text = "Active Directory Name"
+        Me.ADNameRadioButton.UseVisualStyleBackColor = True
+        '
+        'LoginNameComboBox
+        '
+        Me.LoginNameComboBox.Enabled = False
+        Me.LoginNameComboBox.FormattingEnabled = True
+        Me.LoginNameComboBox.Location = New System.Drawing.Point(168, 66)
+        Me.LoginNameComboBox.Name = "LoginNameComboBox"
+        Me.LoginNameComboBox.Size = New System.Drawing.Size(121, 21)
+        Me.LoginNameComboBox.TabIndex = 5
+        '
+        'MachineNameComboBox
+        '
+        Me.MachineNameComboBox.Enabled = False
+        Me.MachineNameComboBox.FormattingEnabled = True
+        Me.MachineNameComboBox.Location = New System.Drawing.Point(168, 102)
+        Me.MachineNameComboBox.Name = "MachineNameComboBox"
+        Me.MachineNameComboBox.Size = New System.Drawing.Size(121, 21)
+        Me.MachineNameComboBox.TabIndex = 8
+        '
+        'ADNameComboBox
+        '
+        Me.ADNameComboBox.Enabled = False
+        Me.ADNameComboBox.FormattingEnabled = True
+        Me.ADNameComboBox.Location = New System.Drawing.Point(168, 138)
+        Me.ADNameComboBox.Name = "ADNameComboBox"
+        Me.ADNameComboBox.Size = New System.Drawing.Size(121, 21)
+        Me.ADNameComboBox.TabIndex = 11
+        '
+        'ClassNameListBox
+        '
+        Me.ClassNameListBox.Enabled = False
+        Me.ClassNameListBox.FormattingEnabled = True
+        Me.ClassNameListBox.Location = New System.Drawing.Point(12, 175)
+        Me.ClassNameListBox.Name = "ClassNameListBox"
+        Me.ClassNameListBox.Size = New System.Drawing.Size(211, 368)
+        Me.ClassNameListBox.TabIndex = 13
+        '
+        'StudentListBox
+        '
+        Me.StudentListBox.Enabled = False
+        Me.StudentListBox.FormattingEnabled = True
+        Me.StudentListBox.Location = New System.Drawing.Point(238, 175)
+        Me.StudentListBox.Name = "StudentListBox"
+        Me.StudentListBox.Size = New System.Drawing.Size(211, 368)
+        Me.StudentListBox.TabIndex = 14
+        '
+        'FolderBrowserDialog1
+        '
+        Me.FolderBrowserDialog1.Description = "Please select the folder containing the LanSchool Class List CSV Files..."
+        Me.FolderBrowserDialog1.ShowNewFolderButton = False
+        '
+        'FolderPathTextBox
+        '
+        Me.FolderPathTextBox.Location = New System.Drawing.Point(12, 27)
+        Me.FolderPathTextBox.Name = "FolderPathTextBox"
+        Me.FolderPathTextBox.Size = New System.Drawing.Size(277, 20)
+        Me.FolderPathTextBox.TabIndex = 1
+        '
+        'BrowseButton
+        '
+        Me.BrowseButton.Location = New System.Drawing.Point(295, 25)
+        Me.BrowseButton.Name = "BrowseButton"
+        Me.BrowseButton.Size = New System.Drawing.Size(75, 23)
+        Me.BrowseButton.TabIndex = 2
+        Me.BrowseButton.Text = "Browse"
+        Me.BrowseButton.UseVisualStyleBackColor = True
+        '
+        'LoadButton
+        '
+        Me.LoadButton.Enabled = False
+        Me.LoadButton.Location = New System.Drawing.Point(376, 25)
+        Me.LoadButton.Name = "LoadButton"
+        Me.LoadButton.Size = New System.Drawing.Size(75, 23)
+        Me.LoadButton.TabIndex = 3
+        Me.LoadButton.Text = "Load"
+        Me.LoadButton.UseVisualStyleBackColor = True
+        '
+        'LoginNameAddNewButton
+        '
+        Me.LoginNameAddNewButton.Enabled = False
+        Me.LoginNameAddNewButton.Location = New System.Drawing.Point(295, 64)
+        Me.LoginNameAddNewButton.Name = "LoginNameAddNewButton"
+        Me.LoginNameAddNewButton.Size = New System.Drawing.Size(75, 23)
+        Me.LoginNameAddNewButton.TabIndex = 6
+        Me.LoginNameAddNewButton.Text = "Add New"
+        Me.LoginNameAddNewButton.UseVisualStyleBackColor = True
+        '
+        'MachineNameAddNewButton
+        '
+        Me.MachineNameAddNewButton.Enabled = False
+        Me.MachineNameAddNewButton.Location = New System.Drawing.Point(295, 100)
+        Me.MachineNameAddNewButton.Name = "MachineNameAddNewButton"
+        Me.MachineNameAddNewButton.Size = New System.Drawing.Size(75, 23)
+        Me.MachineNameAddNewButton.TabIndex = 9
+        Me.MachineNameAddNewButton.Text = "Add New"
+        Me.MachineNameAddNewButton.UseVisualStyleBackColor = True
+        '
+        'ADNameAddNewButton
+        '
+        Me.ADNameAddNewButton.Enabled = False
+        Me.ADNameAddNewButton.Location = New System.Drawing.Point(295, 136)
+        Me.ADNameAddNewButton.Name = "ADNameAddNewButton"
+        Me.ADNameAddNewButton.Size = New System.Drawing.Size(75, 23)
+        Me.ADNameAddNewButton.TabIndex = 12
+        Me.ADNameAddNewButton.Text = "Add New"
+        Me.ADNameAddNewButton.UseVisualStyleBackColor = True
+        '
+        'AddNewClassButton
+        '
+        Me.AddNewClassButton.Enabled = False
+        Me.AddNewClassButton.Location = New System.Drawing.Point(148, 549)
+        Me.AddNewClassButton.Name = "AddNewClassButton"
+        Me.AddNewClassButton.Size = New System.Drawing.Size(75, 23)
+        Me.AddNewClassButton.TabIndex = 16
+        Me.AddNewClassButton.Text = "Add New"
+        Me.AddNewClassButton.UseVisualStyleBackColor = True
+        '
+        'AddNewStudentButton
+        '
+        Me.AddNewStudentButton.Enabled = False
+        Me.AddNewStudentButton.Location = New System.Drawing.Point(374, 549)
+        Me.AddNewStudentButton.Name = "AddNewStudentButton"
+        Me.AddNewStudentButton.Size = New System.Drawing.Size(75, 23)
+        Me.AddNewStudentButton.TabIndex = 18
+        Me.AddNewStudentButton.Text = "Add New"
+        Me.AddNewStudentButton.UseVisualStyleBackColor = True
+        '
+        'ClassCountLabel
+        '
+        Me.ClassCountLabel.AutoSize = True
+        Me.ClassCountLabel.Location = New System.Drawing.Point(12, 554)
+        Me.ClassCountLabel.Name = "ClassCountLabel"
+        Me.ClassCountLabel.Size = New System.Drawing.Size(39, 13)
+        Me.ClassCountLabel.TabIndex = 15
+        Me.ClassCountLabel.Text = "Label1"
+        Me.ClassCountLabel.Visible = False
+        '
+        'StudentCountLabel
+        '
+        Me.StudentCountLabel.AutoSize = True
+        Me.StudentCountLabel.Location = New System.Drawing.Point(235, 554)
+        Me.StudentCountLabel.Name = "StudentCountLabel"
+        Me.StudentCountLabel.Size = New System.Drawing.Size(39, 13)
+        Me.StudentCountLabel.TabIndex = 17
+        Me.StudentCountLabel.Text = "Label2"
+        Me.StudentCountLabel.Visible = False
+        '
+        'LanSchoolClassListsHelper
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(461, 584)
+        Me.Controls.Add(Me.StudentCountLabel)
+        Me.Controls.Add(Me.ClassCountLabel)
+        Me.Controls.Add(Me.AddNewStudentButton)
+        Me.Controls.Add(Me.AddNewClassButton)
+        Me.Controls.Add(Me.ADNameAddNewButton)
+        Me.Controls.Add(Me.MachineNameAddNewButton)
+        Me.Controls.Add(Me.LoginNameAddNewButton)
+        Me.Controls.Add(Me.LoadButton)
+        Me.Controls.Add(Me.BrowseButton)
+        Me.Controls.Add(Me.FolderPathTextBox)
+        Me.Controls.Add(Me.StudentListBox)
+        Me.Controls.Add(Me.ClassNameListBox)
+        Me.Controls.Add(Me.ADNameComboBox)
+        Me.Controls.Add(Me.MachineNameComboBox)
+        Me.Controls.Add(Me.LoginNameComboBox)
+        Me.Controls.Add(Me.ADNameRadioButton)
+        Me.Controls.Add(Me.MachineNameRadioButton)
+        Me.Controls.Add(Me.LoginNameRadioButton)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "LanSchoolClassListsHelper"
+        Me.Text = "LanSchool Class Lists Helper"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LoginNameRadioButton As RadioButton
+    Friend WithEvents MachineNameRadioButton As RadioButton
+    Friend WithEvents ADNameRadioButton As RadioButton
+    Friend WithEvents LoginNameComboBox As ComboBox
+    Friend WithEvents MachineNameComboBox As ComboBox
+    Friend WithEvents ADNameComboBox As ComboBox
+    Friend WithEvents ClassNameListBox As ListBox
+    Friend WithEvents StudentListBox As ListBox
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents FolderPathTextBox As TextBox
+    Friend WithEvents BrowseButton As Button
+    Friend WithEvents LoadButton As Button
+    Friend WithEvents LoginNameAddNewButton As Button
+    Friend WithEvents MachineNameAddNewButton As Button
+    Friend WithEvents ADNameAddNewButton As Button
+    Friend WithEvents AddNewClassButton As Button
+    Friend WithEvents AddNewStudentButton As Button
+    Friend WithEvents ClassCountLabel As Label
+    Friend WithEvents StudentCountLabel As Label
+End Class
---
+++ WindowsApplication1/LanSchoolClassListsHelper.vb
@@ -0,0 +1,274 @@
+﻿Imports System.Data.OleDb
+
+Public Class LanSchoolClassListsHelper
+
+    Private folderOpened As Boolean = False
+    Private folderName As String
+    Private sConn As String
+    Private uniqueID As String
+
+    Private LoginNameTeacherList As New DataTable()
+    Private MachineNameTeacherList As New DataTable()
+    Private ADNameTeacherList As New DataTable()
+
+    Private TeacherClassList As New DataTable()
+    Private ClassStudentList As New DataTable()
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MaximizeBox = False
+        FolderPathTextBox.Text = My.Application.Info.DirectoryPath
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        End
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        OpenBrowseForFolder()
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        AboutBox1.Visible = True
+        AboutBox1.Activate()
+    End Sub
+
+    Private Sub BrowseButton_Click(sender As Object, e As EventArgs) Handles BrowseButton.Click
+        OpenBrowseForFolder()
+    End Sub
+
+    Private Sub OpenBrowseForFolder()
+        Dim result As DialogResult = FolderBrowserDialog1.ShowDialog()
+
+        If (result = DialogResult.OK) Then
+            folderName = FolderBrowserDialog1.SelectedPath
+        End If
+
+        FolderPathTextBox.Text = folderName
+
+        LoadCSVData()
+    End Sub
+
+    Private Sub LoadCSVData()
+        If (folderName <> "") Then
+            LoadButton.Enabled = True
+            If (My.Computer.FileSystem.FileExists(folderName + "\ClassesByTeacherLoginName.csv") And My.Computer.FileSystem.FileExists(folderName + "\StudentsForClassByLoginName.csv")) Then
+                LoginNameRadioButton.Enabled = True
+            Else
+                LoginNameRadioButton.Enabled = False
+                LoginNameComboBox.Enabled = False
+                LoginNameAddNewButton.Enabled = False
+            End If
+            If (My.Computer.FileSystem.FileExists(folderName + "\ClassesByTeacherMachineName.csv") And My.Computer.FileSystem.FileExists(folderName + "\StudentsForClassByMachineName.csv")) Then
+                MachineNameRadioButton.Enabled = True
+            Else
+                MachineNameRadioButton.Enabled = False
+                MachineNameComboBox.Enabled = False
+                MachineNameAddNewButton.Enabled = False
+            End If
+            If (My.Computer.FileSystem.FileExists(folderName + "\ClassesByTeacherADName.csv") And My.Computer.FileSystem.FileExists(folderName + "\StudentsForClassByADName.csv")) Then
+                ADNameRadioButton.Enabled = True
+            Else
+                ADNameRadioButton.Enabled = False
+                ADNameComboBox.Enabled = False
+                ADNameAddNewButton.Enabled = False
+            End If
+        End If
+
+        If (LoginNameRadioButton.Enabled Or MachineNameRadioButton.Enabled Or ADNameRadioButton.Enabled) Then
+            LoadButton.Text = "Reload"
+            sConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & folderName & "\;Extended Properties=""text;HDR=Yes;FMT=Delimited"";"
+        Else
+            LoadButton.Text = "Load"
+        End If
+
+        If (LoginNameRadioButton.Enabled) Then
+            LoginNameTeacherList.Clear()
+
+            Using adapt As New OleDbDataAdapter("SELECT DISTINCT TeacherLoginName FROM [ClassesByTeacherLoginName.csv]", sConn)
+                adapt.Fill(LoginNameTeacherList)
+            End Using
+
+            LoginNameComboBox.DataSource = New BindingSource(LoginNameTeacherList, Nothing)
+            LoginNameComboBox.DisplayMember = "TeacherLoginName"
+        End If
+
+        If (MachineNameRadioButton.Enabled) Then
+            MachineNameTeacherList.Clear()
+
+            Using adapt As New OleDbDataAdapter("SELECT DISTINCT TeacherMachineName FROM [ClassesByTeacherMachineName.csv]", sConn)
+                adapt.Fill(MachineNameTeacherList)
+            End Using
+
+            MachineNameComboBox.DataSource = New BindingSource(MachineNameTeacherList, Nothing)
+            MachineNameComboBox.DisplayMember = "TeacherMachineName"
+        End If
+
+        If (ADNameRadioButton.Enabled) Then
+            ADNameTeacherList.Clear()
+
+            Using adapt As New OleDbDataAdapter("SELECT DISTINCT TeacherADFullName FROM [ClassesByTeacherADName.csv]", sConn)
+                adapt.Fill(ADNameTeacherList)
+            End Using
+
+            ADNameComboBox.DataSource = New BindingSource(ADNameTeacherList, Nothing)
+            ADNameComboBox.DisplayMember = "TeacherADFullName"
+        End If
+
+    End Sub
+
+    Private Sub LoadButton_Click(sender As Object, e As EventArgs) Handles LoadButton.Click
+        folderName = FolderPathTextBox.Text
+
+        LoadCSVData()
+    End Sub
+
+    Private Sub LoginNameRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles LoginNameRadioButton.CheckedChanged
+        If (LoginNameRadioButton.Checked = True) Then
+            MachineNameRadioButton.Checked = False
+            ADNameRadioButton.Checked = False
+
+            LoginNameComboBox.Enabled = True
+            'LoginNameAddNewButton.Enabled = True
+            ClassNameListBox.Enabled = True
+
+            EvaluateClassList()
+
+        Else
+            LoginNameComboBox.Enabled = False
+            LoginNameAddNewButton.Enabled = False
+        End If
+    End Sub
+
+    Private Sub MachineNameRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles MachineNameRadioButton.CheckedChanged
+        If (MachineNameRadioButton.Checked = True) Then
+            LoginNameRadioButton.Checked = False
+            ADNameRadioButton.Checked = False
+
+            MachineNameComboBox.Enabled = True
+            'MachineNameAddNewButton.Enabled = True
+            ClassNameListBox.Enabled = True
+
+            EvaluateClassList()
+        Else
+            MachineNameComboBox.Enabled = False
+            MachineNameAddNewButton.Enabled = False
+        End If
+    End Sub
+
+    Private Sub ADNameRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles ADNameRadioButton.CheckedChanged
+        If (ADNameRadioButton.Checked = True) Then
+            LoginNameRadioButton.Checked = False
+            MachineNameRadioButton.Checked = False
+
+            ADNameComboBox.Enabled = True
+            'ADNameAddNewButton.Enabled = True
+            ClassNameListBox.Enabled = True
+
+            EvaluateClassList()
+        Else
+            ADNameComboBox.Enabled = False
+            ADNameAddNewButton.Enabled = False
+        End If
+    End Sub
+
+    Private Sub FolderPathTextBox_TextChanged(sender As Object, e As EventArgs) Handles FolderPathTextBox.TextChanged
+        If (FolderPathTextBox.Text <> "") Then
+            LoadButton.Enabled = True
+            If (FolderPathTextBox.Text = folderName) Then
+                LoadButton.Text = "Reload"
+            Else
+                LoadButton.Text = "Load"
+            End If
+        End If
+    End Sub
+
+    Private Sub EvaluateClassList()
+        Dim sQuery As String
+        If LoginNameRadioButton.Checked Then
+            sQuery = "SELECT [UniqueClassIdentifier], [Personalized Class Name], [Personalized Class Name]&' ('&[UniqueClassIdentifier]&')' as ClassDisplayName FROM [ClassesByTeacherLoginName.csv] WHERE TeacherLoginName='" & LoginNameComboBox.Text & "' ORDER BY TeacherLoginName"
+        ElseIf MachineNameRadioButton.Checked Then
+            sQuery = "SELECT [UniqueClassIdentifier], [Personalized Class Name], [Personalized Class Name]&' ('&[UniqueClassIdentifier]&')' as ClassDisplayName FROM [ClassesByTeacherMachineName.csv] WHERE TeacherMachineName='" & MachineNameComboBox.Text & "' ORDER BY TeacherMachineName"
+        ElseIf ADNameRadioButton.Checked Then
+            sQuery = "SELECT [UniqueClassIdentifier], [Personalized Class Name], [Personalized Class Name]&' ('&[UniqueClassIdentifier]&')' as ClassDisplayName FROM [ClassesByTeacherADName.csv] WHERE TeacherADFullName='" & ADNameComboBox.Text & "' ORDER BY TeacherADFullName"
+        Else
+            Exit Sub
+        End If
+
+        TeacherClassList.Clear()
+
+        Using adapt As New OleDbDataAdapter(sQuery, sConn)
+            adapt.Fill(TeacherClassList)
+        End Using
+
+        ClassNameListBox.DataSource = TeacherClassList
+        ClassNameListBox.DisplayMember = "ClassDisplayName"
+        ClassNameListBox.ValueMember = "UniqueClassIdentifier"
+
+        ClassCountLabel.Text = "Number of Classes: " & TeacherClassList.Rows.Count.ToString
+        ClassCountLabel.Visible = True
+
+        EvaluateStudentList()
+    End Sub
+
+    Private Sub EvaluateStudentList()
+        If (TeacherClassList.Rows.Count > 0) Then
+            If Not (ClassNameListBox.SelectedValue.ToString = "" Or ClassNameListBox.SelectedValue.ToString = "System.Data.DataRowView") Then
+
+                StudentListBox.Enabled = True
+
+                Dim sQuery As String
+
+                If LoginNameRadioButton.Checked Then
+                    sQuery = "SELECT DISTINCT [StudentLoginName] FROM [StudentsForClassByLoginName.csv] WHERE UniqueClassIdentifier='" & ClassNameListBox.SelectedValue.ToString & "' ORDER BY StudentLoginName"
+                    StudentListBox.DisplayMember = "StudentLoginName"
+                ElseIf MachineNameRadioButton.Checked Then
+                    sQuery = "SELECT DISTINCT [StudentMachineName] FROM [StudentsForClassByMachineName.csv] WHERE UniqueClassIdentifier='" & ClassNameListBox.SelectedValue.ToString & "' ORDER BY StudentMachineName"
+                    StudentListBox.DisplayMember = "StudentMachineName"
+                ElseIf ADNameRadioButton.Checked Then
+                    sQuery = "SELECT DISTINCT [StudentADFullName] FROM [StudentsForClassByADName.csv] WHERE UniqueClassIdentifier='" & ClassNameListBox.SelectedValue.ToString & "' ORDER BY StudentADFullName"
+                    StudentListBox.DisplayMember = "StudentADFullName"
+                Else
+                    Exit Sub
+                End If
+
+                ClassStudentList.Clear()
+
+                Using adapt As New OleDbDataAdapter(sQuery, sConn)
+                    adapt.Fill(ClassStudentList)
+                End Using
+
+                StudentListBox.DataSource = ClassStudentList
+
+                StudentCountLabel.Text = "Number of Students: " & ClassStudentList.Rows.Count.ToString
+                StudentCountLabel.Visible = True
+            End If
+        End If
+
+    End Sub
+
+    Private Sub LoginNameComboBox_SelectedValueChanged(sender As Object, e As EventArgs) Handles LoginNameComboBox.SelectedValueChanged
+        If LoginNameRadioButton.Checked = True Then
+            EvaluateClassList()
+        End If
+    End Sub
+
+    Private Sub MachineNameComboBox_SelectedValueChanged(sender As Object, e As EventArgs) Handles MachineNameComboBox.SelectedValueChanged
+        If MachineNameRadioButton.Checked = True Then
+            EvaluateClassList()
+        End If
+    End Sub
+
+    Private Sub ADNameComboBox_SelectedValueChanged(sender As Object, e As EventArgs) Handles ADNameComboBox.SelectedValueChanged
+        If ADNameRadioButton.Checked = True Then
+            EvaluateClassList()
+        End If
+    End Sub
+
+    Private Sub ClassNameListBox_SelectedValueChanged(sender As Object, e As EventArgs) Handles ClassNameListBox.SelectedValueChanged
+        EvaluateStudentList()
+    End Sub
+
+    Private Sub AddNewStudentButton_Click(sender As Object, e As EventArgs) Handles AddNewStudentButton.Click
+
+    End Sub
+End Class
